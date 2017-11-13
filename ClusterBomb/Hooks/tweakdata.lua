77326ddcdfb8adddfe50345266bb447d46ab686d
Dr_Newbie <tf2baidonation@gmail.com>
2017-11-14 01:37:41 +0800

    mistake

--- ClusterBomb/Hooks/tweakdata.lua
+++ ClusterBomb/Hooks/tweakdata.lua
@@ -1,3 +1,3 @@
 tweak_data.projectiles.frag_cluster = {}
-tweak_data.projectiles.frag_cluster = deep_clone(tweak_data.projectiles.concussion.)
+tweak_data.projectiles.frag_cluster = deep_clone(tweak_data.projectiles.concussion)
 tweak_data.projectiles.frag_cluster.name_id = "bm_throw_frag_cluster"
