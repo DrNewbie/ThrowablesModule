f42392dd4e5b73cc63be03f4134be26639a7904f
Dr_Newbie <tf2baidonation@gmail.com>
2017-11-19 04:04:35 +0800

    4 ; icon
    https://prnt.sc/hc74os

--- mods/BeardLib/Modules/ThrowablesModule.lua
+++ mods/BeardLib/Modules/ThrowablesModule.lua
@@ -17,12 +17,18 @@ function ThrowablesModule:RegisterHook()
 			return
 		end
 		
+		local icon = self._config.icon and "mods_"..BeardLib.Utils.Path:GetFileNameWithoutExtension(self._config.icon)
+		if icon then
+			tweak_data.hud_icons[icon] = {texture = self._config.icon, texture_rect = self._config.icon_rect or false, custom = true}
+		end
+		
 		local data = table.merge(deep_clone(self._config.based_on and (bm_self.projectiles[self._config.based_on] ~= nil and bm_self.projectiles[self._config.based_on]) or bm_self.projectiles.kabar), table.merge({
 			name_id = "bm_throw_" .. self._config.id,
 			dlc = self.defaults.dlc,
 			custom = true,
 			free = true,
-			base_on = self._config.based_on
+			base_on = self._config.based_on,
+			 icon = icon or "pd2_question"
 		}, self._config.item or self._config))
 		dlc = data.dlc
 		if not self._config.texture_bundle_folder then
