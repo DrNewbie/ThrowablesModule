40d5717472f575aa46a712f9360e2d57e085764c
Dr_Newbie <tf2baidonation@gmail.com>
2022-02-05 04:34:56 +0800

    ClusterBomb[4] ; fully client side

--- Module/ClusterBomb/Hooks/fraggrenade.lua
+++ Module/ClusterBomb/Hooks/fraggrenade.lua
@@ -1,5 +1,7 @@
-Hooks:PostHook(FragGrenade, "_detonate", "frag_cluster_detonate", function(self, ...)
-	if tostring(self._new_frag) == 'frag_cluster' then
+ClusterBombFragGrenade = ClusterBombFragGrenade or class(FragGrenade)
+
+Hooks:PreHook(ClusterBombFragGrenade, "_detonate", "frag_cluster_detonate", function(self, ...)
+	if tostring(self._new_frag) == "frag_cluster" and not self.__small_one and AddonProjectileBase then
 		local pos = self._unit:position()
 		local _pos_offset = function (i)
 			local ang = math.random() * 360 * math.pi * i
@@ -7,14 +9,14 @@ Hooks:PostHook(FragGrenade, "_detonate", "frag_cluster_detonate", function(self,
 			return Vector3(math.cos(ang) * rad, math.sin(ang) * rad, 0)
 		end
 		local _l = {
-			"frag",
-			"frag",
-			"frag",
-			"frag",
-			"frag",
-			"frag",
-			"frag",
-			"frag"
+			"frag_cluster",
+			"frag_cluster",
+			"frag_cluster",
+			"frag_cluster",
+			"frag_cluster",
+			"frag_cluster",
+			"frag_cluster",
+			"frag_cluster"
 		}
 		local _xy = {
 			Vector3(0, 0, 0),
@@ -29,9 +31,20 @@ Hooks:PostHook(FragGrenade, "_detonate", "frag_cluster_detonate", function(self,
 		}
 		local user = self:thrower_unit() or self._unit
 		for i = 1, #_l do
-			DelayedCalls:Add('ClusterBomb_Spilt_'.. tostring(user:key()) ..'_' .. i, i*0.15, function()
-				ProjectileBase.throw_projectile(_l[i], pos + _pos_offset(i), Vector3(0, 0, 1) + _xy[i])
+			DelayedCalls:Add('ClusterBomb_Spilt_'.. tostring(user) ..'_' .. i, i*0.15, function()
+				local small_one = AddonProjectileBase.throw_projectile(_l[i], pos + _pos_offset(i), Vector3(0, 0, 0.5) + _xy[i], managers.network:session():local_peer():id())
+				if small_one and small_one.base and small_one:base() then
+					small_one:base().__small_one = true
+					small_one:base()._range = 400
+					small_one:base()._timer = 2
+					small_one:base()._damage = 5
+					small_one:base()._player_damage = 1
+				end
 			end)
 		end
 	end
 end)
+
+function ClusterBombFragGrenade:bullet_hit()
+
+end
